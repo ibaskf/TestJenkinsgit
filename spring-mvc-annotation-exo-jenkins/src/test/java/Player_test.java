8672a25214d424c2c4f1e8fa21299a25255fcb3e
stagiaire <stagiaire@Stagiaire03>
2015-09-07 15:36:58 +0200

    t

---
+++ spring-mvc-annotation-exo-jenkins/src/test/java/Player_test.java
@@ -0,0 +1,27 @@
+
+import fr.treeptik.dao.PlayerDAO;
+import  fr.treeptik.model.Player;
+import fr.treeptik.service.PlayerService;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.*;
+import static org.junit.Test.*;
+
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+public class Player_test {
+
+//	 PlayerService playerservice=new PlayerService();
+	 
+	// Player player =new Player();
+	
+	
+	@Test
+	public void Test_jenkins(){
+ 	assertTrue(false);
+
+	}
+
+}
