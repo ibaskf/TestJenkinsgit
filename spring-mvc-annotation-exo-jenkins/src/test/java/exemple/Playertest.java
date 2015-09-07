c8cbce0fa95b4d405e96102b382158d2b6bb1d1f
stagiaire <stagiaire@Stagiaire03>
2015-09-07 15:58:17 +0200

    d

---
+++ spring-mvc-annotation-exo-jenkins/src/test/java/exemple/Playertest.java
@@ -0,0 +1,21 @@
+
+package exemple;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class Playertest {
+
+//	 PlayerService playerservice=new PlayerService();
+	 
+	// Player player =new Player();
+	
+	
+	@Test
+	public void Test_jenkins(){
+ 	assertTrue(false);
+
+	}
+
+}
